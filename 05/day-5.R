110de257a28439f3da84c9e866bf2f8a062132c4
Jenn Schilling <jathorne2@gmail.com>
2020-12-05 17:03:55 -0700

    day 5 part 2 completed

--- 05/day-5.R
+++ 05/day-5.R
@@ -89,3 +89,27 @@ input_calc <- input_calc %>%
   mutate(seat_id = row * 8 + col)
 
 print(max(input_calc$seat_id)) # Answer
+
+# Part 2
+
+# It's a completely full flight, so your seat should be the only missing 
+# boarding pass in your list. However, there's a catch: some of the seats 
+# at the very front and back of the plane don't exist on this aircraft, so 
+# they'll be missing from your list as well.
+
+# Your seat wasn't at the very front or back, though; the seats with IDs +1
+# and -1 from yours will be in your list.
+
+# What is the ID of your seat?
+
+# Search through the list and find the ID that is missing
+
+input_calc_2 <- input_calc %>% 
+  arrange(seat_id) %>%
+  mutate(lag = lag(seat_id),
+         check = lag + 1 == seat_id) %>%
+  filter(check == FALSE) %>%
+  select(seat_id)
+
+print(input_calc_2$seat_id - 1) # Answer
+  
