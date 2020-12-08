7defd790f443d84dae8d206f4069a1650c223873
jaschilling <jaschilling@arizona.edu>
2020-12-07 20:44:22 -0700

    partial solution to part 2 but need to adjust number of bag logic (need to multiply and add)

--- 07/day-7.R
+++ 07/day-7.R
@@ -72,5 +72,75 @@ nrow(all_shiny_gold) - 1 # subtract the rule for "shiny gold bags contain.."
 
 # Part 2 - How many individual bags are required inside your single shiny gold bag?
 
+# Iteratively get the number of bags required
+
+
+find_req_bags <- function(num_bags, req_bags){
+  
+  # Remove "contains" first portion of string and period
+  req_bags <- req_bags %>%
+    mutate(bag_rule = str_remove(bag_rule, ".*contain ")) %>%
+    mutate(bag_rule = str_remove(bag_rule, "\\."))
+  
+  # Get list of bags inside the bags
+  bag_list <- unlist(str_split(req_bags$bag_rule, ", "))
+  
+  new_req_bags <- data.frame()
+  
+  # Compute number of bags inside the required bags
+  # Get the next set of required bags
+  for(i in 1:length(bag_list)){
+    
+    if(bag_list[i] != "no other bags"){
+    
+       # Increment number of bags ### PROBLEM - cannot just add number of bags, it's multiplicative
+       num_bags <- num_bags + parse_number(bag_list[i])
+       
+       # Get bag details
+       outer_bag <- word(bag_list[i], 2, 4, sep = " ")
+       
+       if(substr(outer_bag, str_length(outer_bag), str_length(outer_bag)) != 's'){
+         
+         outer_bag <- paste0(outer_bag, 's')
+         
+       }
+       
+       outer_bag <- paste0("^", outer_bag)
+       
+       # Add to list of bag requirements
+       new_req_bags <- rbind(new_req_bags,
+                             rules %>% filter(str_detect(bag_rule, outer_bag)))
+       
+      }
+    
+    }
+    
+  result <- list(num_bags, new_req_bags)
+  
+  return(result)
+  
+}
+
+
+# Initialize Variables 
+num_bags <- 0
+
+rules <- input %>%
+  rename(bag_rule = X1) 
+
+req_bags <- rules %>%
+  filter(str_detect(bag_rule, "^shiny gold bag"))
+
+# Search for bags in bags
+while(!is_empty(req_bags)){
+  
+  n <- find_req_bags(num_bags, req_bags)
+  
+  num_bags <- n[[1]]
+  
+  req_bags <- n[[2]]
+  
+  
+}
 
 
